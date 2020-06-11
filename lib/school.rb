bf8296cb1ca9d92db6cf8d13df1f5411f7919d3f
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-06-11 19:52:54 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    # attr_accessor :name 
+    # attr_reader :grade, :sort
+
+
+    def initialize(name)
+        @name = name 
+        @roster = {} 
+     end 
+
+     def add_student(name, grade)
+        if @roster[grade] 
+            @roster[grade] = [name]
+        else 
+            @roster[grade][name]  
+        end       
+     end 
+
+     def roster 
+        @roster 
+     end 
+end 
+
+# new_hash = {} 
+# new_hash{:roster => 1} 
+
+# Hash.ne
